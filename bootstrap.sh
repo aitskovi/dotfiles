44203e56b3d2e4528cd289db142d18a77181cf1d
Avi Itskovich <avi@inkling.com>
2011-09-25 17:34:27 -0700

    Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+# Back up old dotfiles
+mv ~/.bashrc ~/.bashrc.backup
+mv ~/.vimrc.local ~/.vimrc.local.backup
+mv ~/.gvimrc.local ~/.gvimrc.local.backup
+
+dir=`pwd`
+
+# Link to new dotfiles in this repo
+ln -s ${dir}/.bashrc ~/.bashrc
+ln -s ${dir}/.vimrc.local ~/.vimrc.local
+ln -s ${dir}/.gvimrc.local ~/.gvimrc.local
